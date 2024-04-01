dc4fe2e0a0cace2b4b2efaedbef9e007092e9d48
Digital <digital@Macbooks-MacBook-Pro.local>
2024-04-01 16:39:50 +0545

    First commit

--- Basic_routing.py
+++ Basic_routing.py
@@ -1,15 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template,url_for
 app = Flask(__name__)
+
+#We are going to create a dummy data
+#Data it related to list of people and there post 
+
+posts =[
+   {
+      'author' : 'Niraj Baral',
+      'title' : 'Blog Post 1',
+      'content' : 'First post content',
+      'date_posted' : 'April 20,2018'
+   },
+   {
+      'author' : 'Ozzu Bhandari',
+      'title' : 'Blog Post 2',
+      'content' : 'Secont post content',
+      'date_posted' : 'May 12,2018'
+   }
+
+]
 
 @app.route('/')
 @app.route('/home')
 def hello_world():
-   return "<h1>Hello World To flask</h1>"
+   return render_template('home.html',title ='Home Page' ,posts= posts)
 
 
 @app.route("/about")
 def about():
-   return "<h1>About page</h1>"
+   return render_template('about.html', title ='About')
 
 
 
