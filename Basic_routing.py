0ef63c634dd5da5db9b960aa04d9a9851370fe25
Digital <digital@Macbooks-MacBook-Pro.local>
2024-04-02 16:18:24 +0545

    First Commit

--- Basic_routing.py
+++ Basic_routing.py
@@ -1,43 +1,56 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
-#We are going to create a dummy data
-#Data it related to list of people and there post 
+app.config['SECRET_KEY'] = '4c28ab3693403430f67b931feab64dae'
 
-posts =[
-   {
-      'author' : 'Niraj Baral',
-      'title' : 'Blog Post 1',
-      'content' : 'First post content',
-      'date_posted' : 'April 20,2018'
-   },
-   {
-      'author' : 'Ozzu Bhandari',
-      'title' : 'Blog Post 2',
-      'content' : 'Secont post content',
-      'date_posted' : 'May 12,2018'
-   }
-
+posts = [
+    {
+        'author': 'Niraj Baral',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Ozzu Bhandari',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
 ]
 
-@app.route('/')
-@app.route('/home')
-def hello_world():
-   return render_template('home.html',title ='Home Page' ,posts= posts)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-   return render_template('about.html', title ='About')
+    return render_template('about.html', title='About')
 
+@app.route("/register", methods =['Get','POST'])  # Route for registration
+def register(): 
+        #create an instance 
+    form = RegistrationForm()
+    #Validate submit/Post method
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title ='Register', form =form)
 
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data =='password':
+          flash(f'You have been logged in!', 'success')
+          return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password','danger' )
+    
+    return render_template('login.html', title = 'Login', form = form)
 
-#if we don't want  to make that env setup run each 
-#time we can do that by rumming app in python directly  
-
-
-
-if __name__ == '__main__':  ## it is main run file 
-   app.run(debug=True)    # Basically now we can run by typing python main.py 
-                           # instade flask run main.py 
-
+if __name__ == '__main__':
+    app.run(debug=True)
